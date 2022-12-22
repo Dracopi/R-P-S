d3a57e58592f3f34c0cdcb7002ad35c3212164e0
Dracopi <dracopiart@gmail.com>
2022-12-22 10:26:12 -0400

    Wrote base pseudocode

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// Pseudocodigo guia 
+
+// Paso 1
+// Obtener la información del usuario
+
+// Paso 2
+// Darle a escoger a la maquina
+
+// Paso 3
+// Ver cual de los dos es ganador
+
+// Paso 4
+// Mostrarlo en pantalla
+
+// Paso 5
+// Repetir Paso 1-4 hasta tener un total de 5 rondas
+
+// Paso 6
+// Identificar al ganador total
